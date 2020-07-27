332bae7c9d644bd8a53931adae49b0a86e097142
Zach Bryan <zacharybryan@icloud.com>
2020-07-27 08:28:56 -0700

    pseudo code

--- script.js
+++ script.js
@@ -6,11 +6,13 @@
 
 var timerElement = document.getElementById("quizTimer");
 var buttonElement = document.getElementById("startButton");
+var welcomeElement = document.getElementById("welcomeContainer")
 
 var quizTimer = 60;
 
 buttonElement.addEventListener("click", function(){
     prepareQuiz();
+    // var welcomeElement = display question 1 
 })
 
 function prepareQuiz() {
@@ -29,3 +31,13 @@ function prepareQuiz() {
 document.appendChild.navId;
 navId.appendChild.quizTimer;
 
+// Get questions (5) Mulitple Choice 
+// apply score changes/ subtract from timer for incorect answers
+// take final timer amount and create score
+// add high score
+// commit it to the memory thingy 
+// pull form the memory to display the high scores added by initals
+// Look at activity with commiting memory 
+// put final touches on apperance (look at portfolio looks) (d-flex maybe or something like that )
+// look into adding sound effects
+// create a readME 
